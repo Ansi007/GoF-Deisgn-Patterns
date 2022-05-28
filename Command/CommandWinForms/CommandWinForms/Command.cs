487da9e83f1621674ae8d55869e82a8fefde2f5c
Anns Shahbaz <annsshahbaz@gmail.com>
2022-05-28 18:41:55 +0500

    Player Class Added

--- Command/CommandWinForms/CommandWinForms/Command.cs
+++ Command/CommandWinForms/CommandWinForms/Command.cs
@@ -3,9 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CommandWinForms
 {
+    public class Player
+    {
+        public int posX, posY;
+        public Player(Label dotLabel)
+        {
+            posX = dotLabel.Location.X;
+            posY = dotLabel.Location.Y;
+        }
+        public void Move(int posX, int posY)
+        {
+            this.posX = posX;
+            this.posY = posY;
+        }
+    }
     public interface Command
     {
         void execute();
